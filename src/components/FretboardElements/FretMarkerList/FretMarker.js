ebef4503d01b0e0e5c7884bf2568fa69a5e93ca6
Remy Turpault <remy.turpault@gmail.com>
2020-08-25 23:35:00 +0200

    rewrote styles for FretMarkerList/FretMarker in SCSS

--- src/components/FretboardElements/FretMarkerList/FretMarker.js
+++ src/components/FretboardElements/FretMarkerList/FretMarker.js
@@ -1,44 +1,10 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles(() => ({
-  fretMarker: {
-    marginRight: "1em",
-    boxSizing: "content-box",
-    borderBottom: "0.25em solid transparent",
-    height: (props) =>
-      props.position === 0 ? "2.5em" : `${6 * 0.9438 ** props.position}em`,
-    minHeight: "2.35em",
-    display: "flex",
-    flexFlow: "column nowrap",
-    justifyContent: "center",
-    "& small": {
-      color: "#aaa",
-    },
-    "&:last-child": {
-      borderBottom: "none",
-    },
-    "@media (orientation: landscape)": {
-      height: "auto!important",
-      minHeight: "auto!important",
-      textAlign: "center",
-      marginRight: 0,
-      borderBottom: 0,
-      borderRight: (props) =>
-        props.isLeftHanded ? 0 : "0.25em solid transparent",
-      borderLeft: (props) =>
-        props.isLeftHanded ? "0.25em solid transparent" : 0,
-      width: (props) =>
-        props.position === 0 ? "2.5em" : `${6 * 0.9438 ** props.position}em`,
-      minWidth: "2.35em",
-    },
-  },
-}));
 
 const FretMarker = ({ position, isLeftHanded }) => {
-  const classes = useStyles({ position, isLeftHanded });
+  const fretMarkerClasses = ["FretMarker", `fret-${position}`];
+  if (isLeftHanded) fretMarkerClasses.push("leftHanded");
   return (
-    <div className={classes.fretMarker}>
+    <div className={fretMarkerClasses.join(" ")}>
       <small>{position}</small>
     </div>
   );
--- src/components/FretboardElements/FretMarkerList/FretMarkerList.js
+++ src/components/FretboardElements/FretMarkerList/FretMarkerList.js
@@ -2,28 +2,7 @@ import React from "react";
 
 import FretMarker from "./FretMarker";
 
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles(() => ({
-  fretMarkerList: {
-    marginTop: "2.5em",
-    width: "2em",
-    textAlign: "right",
-    "@media (orientation: landscape)": {
-      marginTop: 0,
-      marginLeft: (props) => (props.isLeftHanded ? 0 : "2.5em"),
-      marginRight: (props) => (props.isLeftHanded ? "2.5em" : 0),
-      display: "flex",
-      flexFlow: (props) =>
-        `${props.isLeftHanded ? "row-reverse" : "row"} nowrap`,
-      width: "auto",
-      height: "2em",
-    },
-  },
-}));
-
 const FretMarkerList = (props) => {
-  const classes = useStyles(props);
   const fretMarkerList = [...Array(props.nbFrets)].map((_, v) => (
     <FretMarker
       key={`fretMarker${v + 1}`}
@@ -31,7 +10,9 @@ const FretMarkerList = (props) => {
       isLeftHanded={props.isLeftHanded}
     />
   ));
-  return <div className={classes.fretMarkerList}>{fretMarkerList}</div>;
+  const fretMarkerClasses = ["FretMarkerList"];
+  if (props.isLeftHanded) fretMarkerClasses.push("leftHanded");
+  return <div className={fretMarkerClasses.join(" ")}>{fretMarkerList}</div>;
 };
 
 export default FretMarkerList;
