081cb4f30f5bc383c3b132e8b940a3d753989910
Remy Turpault <remy.turpault@gmail.com>
2020-09-18 04:37:34 +0200

    Pickerforms styles refactored in SCSS

--- src/containers/ChordPickerForm.js
+++ src/containers/ChordPickerForm.js
@@ -12,101 +12,6 @@ import Button from "@material-ui/core/Button";
 import Fade from "@material-ui/core/Fade";
 import useNoteNames from "hooks/noteNames";
 
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    margin: theme.spacing(4),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    backgroundColor: theme.palette.background.main,
-    [`${theme.breakpoints.up("sm")} and (orientation: portrait)`]: {
-      minHeight: "calc(100vh - 262px)",
-      display: "flex",
-      flexFlow: "column nowrap",
-      justifyContent: "center",
-    },
-    [`${theme.breakpoints.down("sm")} and (orientation: landscape)`]: {
-      margin: "16px 32px",
-    },
-    "@media (max-height: 679px) and (orientation: landscape)": {
-      minHeight: "calc(100vh - 150px)",
-      display: "flex",
-      flexFlow: "column nowrap",
-      justifyContent: "center",
-    },
-    "@media (max-height: 679px) and (min-width: 1024px) and (orientation: landscape)": {
-      minHeight: "calc(100vh - 268px)",
-    },
-    "@media (min-height: 680px) and (orientation: landscape)": {
-      margin: "0 0 0 6px",
-      minHeight: "16.6em",
-    },
-    "@media (min-height: 840px) and (orientation: landscape)": {
-      minHeight: "19.7em" /*"23.1em",*/,
-    },
-  },
-  paperInner: {
-    [`${theme.breakpoints.up("sm")} and (orientation: portrait)`]: {
-      height: "30.8em",
-    },
-    "@media (max-height: 679px) and (orientation: landscape)": {
-      height: "23.1em",
-    },
-    "@media (max-height: 679px) and (min-width: 1024px) and (orientation: landscape)": {
-      height: "27em",
-    },
-  },
-  title: {
-    marginBottom: theme.spacing(2),
-    [`${theme.breakpoints.up("sm")} and (orientation: portrait)`]: {
-      marginBottom: theme.spacing(4),
-    },
-    "@media (min-height: 680px) and (max-height: 839px) and (orientation: landscape)": {
-      display: "none",
-    },
-  },
-  form: {
-    width: 250,
-    margin: "0 auto",
-    [theme.breakpoints.up("sm")]: {
-      width: 400,
-    },
-  },
-  buttonWrapper: {
-    [`${theme.breakpoints.up("sm")} and (orientation: portrait)`]: {
-      marginTop: 8,
-    },
-  },
-  submitButton: {
-    width: 250,
-    marginTop: 16,
-    padding: "16px 0",
-    [theme.breakpoints.up("sm")]: {
-      width: 400,
-    },
-    "& sup": {
-      verticalAlign: "top",
-      position: "relative",
-      top: "-0.25em",
-    },
-    textTransform: "none",
-    [`${theme.breakpoints.down("sm")} and (orientation: landscape)`]: {
-      marginTop: 8,
-      padding: "8px 0",
-    },
-    "@media (min-height: 680px) and (orientation: landscape)": {
-      marginTop: 8,
-      padding: "8px 0",
-    },
-    "@media (min-width: 1590px) and (max-aspect-ratio: 8/5)": {
-      marginTop: 16,
-      padding: "16px 0",
-    },
-  },
-}));
-
 const ChordPickerForm = () => {
   const [quality, setQuality] = useState("");
   const [seventh, setSeventh] = useState("");
@@ -120,8 +25,6 @@ const ChordPickerForm = () => {
   const noteNaming = useSelector((state) => state.settings.noteNaming);
 
   const { getNoteName } = useNoteNames(noteNaming);
-
-  const classes = useStyles();
 
   const onUpdateRoot = (rootNote) => {
     return dispatch(actions.updateRoot(rootNote, noteNaming));
@@ -147,10 +50,10 @@ const ChordPickerForm = () => {
   const rootName = getNoteName(rootNote, useFlats);
 
   return (
-    <div className={`${classes.paper} pickerForm`}>
-      <div className={classes.paperInner}>
+    <div className='paper pickerForm chordPickerForm'>
+      <div className='paperInner'>
         <Typography
-          className={classes.title}
+          className='title'
           variant='h5'
           component='h2'
           align='center'
@@ -172,7 +75,7 @@ const ChordPickerForm = () => {
               selected={selected}
               updateQuality={onUpdateQuality}
               setTmpChordName={setTmpChordName}
-              className={classes.quality}
+              className='quality'
             />
           </div>
         </Fade>
@@ -200,9 +103,9 @@ const ChordPickerForm = () => {
         </Fade>
         <Fade in={quality !== ""} mountOnEnter unmountOnExit timeout={700}>
           <div>
-            <FormControl className={classes.buttonWrapper}>
+            <FormControl className='buttonWrapper'>
               <Button
-                className={classes.submitButton}
+                className='submitButton'
                 variant='contained'
                 color='primary'
                 size='large'
--- src/containers/ScalePickerForm.js
+++ src/containers/ScalePickerForm.js
@@ -14,192 +14,7 @@ import Fade from "@material-ui/core/Fade";
 import useNoteNames from "hooks/noteNames";
 import { computeModeSemitones } from "utility/intervals";
 
-import { makeStyles } from "@material-ui/core/styles";
-
 import scales from "data/scales";
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    margin: theme.spacing(4),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    backgroundColor: theme.palette.background.main,
-    [`${theme.breakpoints.up("sm")} and (orientation: portrait)`]: {
-      minHeight: "calc(100vh - 262px)",
-      display: "flex",
-      flexFlow: "column nowrap",
-      justifyContent: "center",
-    },
-    [`${theme.breakpoints.down("sm")} and (orientation: landscape)`]: {
-      margin: "16px 32px",
-    },
-    "@media (max-height: 679px) and (orientation: landscape)": {
-      minHeight: "calc(100vh - 150px)",
-      display: "flex",
-      flexFlow: "column nowrap",
-      justifyContent: "center",
-    },
-    "@media (max-height: 679px) and (min-width: 1024px) and (orientation: landscape)": {
-      minHeight: "calc(100vh - 268px)",
-    },
-    "@media (min-height: 680px) and (orientation: landscape)": {
-      margin: "0 0 0 6px",
-      minHeight: "9.8em",
-    },
-    "@media (min-height: 840px) and (orientation: landscape)": {
-      minHeight: "13em",
-    },
-  },
-  paperInner: {
-    [`${theme.breakpoints.up("sm")} and (orientation: portrait)`]: {
-      height: "30.8em",
-    },
-    "@media (max-height: 679px) and (orientation: landscape)": {
-      height: "23.1em",
-    },
-    "@media (max-height: 679px) and (min-width: 1024px) and (orientation: landscape)": {
-      height: "27em",
-    },
-  },
-  title: {
-    marginBottom: theme.spacing(2),
-    [`${theme.breakpoints.up("sm")} and (orientation: portrait)`]: {
-      marginBottom: theme.spacing(4),
-    },
-    "@media (min-height: 680px) and (max-height: 839px) and (orientation: landscape)": {
-      display: "none",
-    },
-  },
-  form: {
-    width: 250,
-    margin: "0 auto",
-    [theme.breakpoints.up("sm")]: {
-      width: 400,
-    },
-  },
-  wrapper: {
-    [`${theme.breakpoints.up("sm")} and (orientation: portrait)`]: {
-      margin: "8px 0",
-    },
-  },
-  formControl: {
-    margin: 0,
-    minWidth: 250,
-    [theme.breakpoints.up("sm")]: {
-      minWidth: 400,
-    },
-    [`${theme.breakpoints.down("sm")} and (orientation: landscape)`]: {
-      "& > .MuiTextField-root": {
-        margin: "8px 0",
-      },
-    },
-    "@media (min-height: 680px) and (max-width: 1589px) and (orientation: landscape)": {
-      "& > .MuiTextField-root": {
-        margin: "8px 0",
-      },
-    },
-  },
-  select: {
-    [`${theme.breakpoints.down("sm")} and (orientation: portrait)`]: {
-      "& > .MuiSelect-root > span": {
-        display: "none",
-      },
-    },
-    [`${theme.breakpoints.down("sm")} and (orientation: landscape)`]: {
-      fontSize: "0.9em",
-      "& > .MuiSelect-root": {
-        padding: 12,
-        paddingRight: 32,
-      },
-    },
-    "@media (min-height: 680px) and (max-width: 1589px) and (orientation: landscape)": {
-      fontSize: "0.9em",
-      "& > .MuiSelect-root": {
-        padding: 12,
-        paddingRight: 32,
-      },
-    },
-  },
-  label: {
-    [`${theme.breakpoints.down("sm")} and (orientation: landscape)`]: {
-      fontSize: "1em",
-      transform: "translate(14px, 14px) scale(1)",
-      "&.MuiInputLabel-shrink": {
-        transform: "translate(11px, -6px) scale(0.7)",
-      },
-    },
-    "@media (min-height: 680px) and (max-width: 1589px) and (orientation: landscape)": {
-      fontSize: "1em",
-      transform: "translate(14px, 14px) scale(1)",
-      "&.MuiInputLabel-shrink": {
-        transform: "translate(11px, -6px) scale(0.7)",
-      },
-    },
-  },
-  menu: {
-    [`${theme.breakpoints.down("sm")} and (orientation: landscape)`]: {
-      "& li": {
-        fontSize: "1em!important",
-      },
-    },
-    "@media (min-height: 680px) and (max-width: 1589px) and (orientation: landscape)": {
-      "& li": {
-        fontSize: "1em!important",
-      },
-    },
-  },
-  group: {
-    fontSize: "1em",
-    textAlign: "center",
-    color: theme.palette.gray.main,
-    "& span": {
-      fontSize: "0.85em",
-    },
-  },
-  option: {
-    fontSize: "1em",
-    minHeight: 34,
-    /*"& span": {
-      color: theme.palette.gray.main,
-      fontSize: "0.85em",
-      [theme.breakpoints.down("xs")]: {
-        display: "none",
-      },
-    },*/
-  },
-  buttonWrapper: {
-    [`${theme.breakpoints.up("sm")} and (orientation: portrait)`]: {
-      marginTop: 8,
-    },
-  },
-  submitButton: {
-    width: 250,
-    marginTop: 16,
-    padding: "16px 0",
-    [theme.breakpoints.up("sm")]: {
-      width: 400,
-    },
-    "& sup": {
-      verticalAlign: "top",
-      position: "relative",
-      top: "-0.25em",
-    },
-    textTransform: "none",
-    [`${theme.breakpoints.down("sm")} and (orientation: landscape)`]: {
-      marginTop: 8,
-      padding: "8px 0",
-    },
-    "@media (min-height: 680px) and (orientation: landscape)": {
-      marginTop: 8,
-      padding: "8px 0",
-    },
-    "@media (min-width: 1590px) and (max-aspect-ratio: 8/5)": {
-      marginTop: 16,
-      padding: "16px 0",
-    },
-  },
-}));
 
 const ScalePickerForm = ({ onPick }) => {
   const dispatch = useDispatch();
@@ -208,7 +23,6 @@ const ScalePickerForm = ({ onPick }) => {
   const noteNaming = useSelector((state) => state.settings.noteNaming);
   const [tmpScaleInfo, setTmpScaleInfo] = useState(null);
   const { getNoteName } = useNoteNames(noteNaming);
-  const classes = useStyles();
 
   const rootName = getNoteName(rootNote, useFlats);
 
@@ -267,7 +81,7 @@ const ScalePickerForm = ({ onPick }) => {
         <MenuItem
           key={el.name}
           value={el.fullName}
-          className={classes.option}
+          className='option'
           data-category={cat.category}
         >
           {el.fullName.replace(" scale", "")}
@@ -276,7 +90,7 @@ const ScalePickerForm = ({ onPick }) => {
       );
     });
     return [
-      <ListSubheader key={cat.category} className={classes.group} disableSticky>
+      <ListSubheader key={cat.category} className='group' disableSticky>
         {cat.category}
         {cat.hasOwnProperty("subtitle") && (
           <span>&nbsp;{`(${cat.subtitle})`}</span>
@@ -287,10 +101,10 @@ const ScalePickerForm = ({ onPick }) => {
   });
 
   return (
-    <div className={`${classes.paper} pickerForm`}>
-      <div className={classes.paperInner}>
+    <div className='paper pickerForm scalePickerForm'>
+      <div className='paperInner'>
         <Typography
-          className={classes.title}
+          className='title'
           variant='h5'
           component='h2'
           align='center'
@@ -305,14 +119,14 @@ const ScalePickerForm = ({ onPick }) => {
           toggleFlats={onToggleFlats}
         />
         <Fade in={rootNote !== ""} mountOnEnter unmountOnExit timeout={700}>
-          <div className={classes.wrapper}>
-            <FormControl variant='outlined' className={classes.formControl}>
+          <div className='scaleFieldWrapper'>
+            <FormControl variant='outlined' className='formControl'>
               <TextField
                 variant='outlined'
                 id='scale'
                 select
                 label='Scale'
-                className={classes.textField}
+                className='textField'
                 value={tmpScaleInfo !== null ? tmpScaleInfo.fullName : ""}
                 onChange={(e) => {
                   if (e.target.value !== undefined) {
@@ -325,13 +139,13 @@ const ScalePickerForm = ({ onPick }) => {
                   }
                 }}
                 SelectProps={{
-                  className: classes.select,
+                  className: "select",
                   MenuProps: {
-                    classes: { list: classes.menu },
+                    classes: { list: "menu menuScale" },
                   },
                 }}
                 InputLabelProps={{
-                  className: classes.label,
+                  className: "label",
                 }}
                 margin='normal'
               >
@@ -347,9 +161,9 @@ const ScalePickerForm = ({ onPick }) => {
           timeout={700}
         >
           <div>
-            <FormControl className={classes.buttonWrapper}>
+            <FormControl className='buttonWrapper'>
               <Button
-                className={classes.submitButton}
+                className='submitButton'
                 variant='contained'
                 color='primary'
                 size='large'
