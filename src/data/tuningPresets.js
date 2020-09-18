12b46b9ef6f994e8dd1c086496ec4aab79d27750
Remy Turpault <remy.turpault@gmail.com>
2020-09-18 02:59:53 +0200

    Tuning components refactor (SCSS, proptypes, externalize data)

---
+++ src/data/tuningPresets.js
@@ -0,0 +1,387 @@
+const tuningPresets = [
+  {
+    cat_name: "Standard and derived",
+    tunings: [
+      {
+        id: "EADBGE",
+        name: "Standard",
+        tuning: [
+          { stringId: 6, note: 4, octave: 2 },
+          { stringId: 5, note: 9, octave: 2 },
+          { stringId: 4, note: 2, octave: 3 },
+          { stringId: 3, note: 7, octave: 3 },
+          { stringId: 2, note: 11, octave: 3 },
+          { stringId: 1, note: 4, octave: 4 },
+        ],
+      },
+      {
+        id: "DADGBE",
+        name: "Drop D",
+        tuning: [
+          { stringId: 6, note: 2, octave: 2 },
+          { stringId: 5, note: 9, octave: 2 },
+          { stringId: 4, note: 2, octave: 3 },
+          { stringId: 3, note: 7, octave: 3 },
+          { stringId: 2, note: 11, octave: 3 },
+          { stringId: 1, note: 4, octave: 4 },
+        ],
+      },
+      {
+        id: "DADBGD",
+        name: "Double drop D",
+        tuning: [
+          { stringId: 6, note: 2, octave: 2 },
+          { stringId: 5, note: 9, octave: 2 },
+          { stringId: 4, note: 2, octave: 3 },
+          { stringId: 3, note: 7, octave: 3 },
+          { stringId: 2, note: 11, octave: 3 },
+          { stringId: 1, note: 2, octave: 4 },
+        ],
+      },
+      {
+        id: "BEADF♯B",
+        name: "Baritone",
+        tuning: [
+          { stringId: 6, note: 11, octave: 1 },
+          { stringId: 5, note: 4, octave: 2 },
+          { stringId: 4, note: 9, octave: 2 },
+          { stringId: 3, note: 2, octave: 3 },
+          { stringId: 2, note: 6, octave: 3 },
+          { stringId: 1, note: 11, octave: 3 },
+        ],
+      },
+    ],
+  },
+  {
+    cat_name: "Tunings in C",
+    tunings: [
+      {
+        id: "CGCGCE",
+        name: "Open C major",
+        tuning: [
+          { stringId: 6, note: 0, octave: 2 },
+          { stringId: 5, note: 7, octave: 2 },
+          { stringId: 4, note: 0, octave: 3 },
+          { stringId: 3, note: 7, octave: 3 },
+          { stringId: 2, note: 0, octave: 4 },
+          { stringId: 1, note: 4, octave: 4 },
+        ],
+      },
+      {
+        id: "CGCGCE♭",
+        name: "Open C minor",
+        tuning: [
+          { stringId: 6, note: 0, octave: 2 },
+          { stringId: 5, note: 7, octave: 2 },
+          { stringId: 4, note: 0, octave: 3 },
+          { stringId: 3, note: 7, octave: 3 },
+          { stringId: 2, note: 0, octave: 4 },
+          { stringId: 1, note: 3, octave: 4 },
+        ],
+      },
+      {
+        id: "CGCGGC",
+        name: "Open C no third / Ben Howard",
+        tuning: [
+          { stringId: 6, note: 0, octave: 2 },
+          { stringId: 5, note: 7, octave: 2 },
+          { stringId: 4, note: 0, octave: 3 },
+          { stringId: 3, note: 7, octave: 3 },
+          { stringId: 2, note: 7, octave: 3 },
+          { stringId: 1, note: 0, octave: 4 },
+        ],
+      },
+    ],
+  },
+  {
+    cat_name: "Tunings in D",
+    tunings: [
+      {
+        id: "DADGAD",
+        name: "Dsus4",
+        tuning: [
+          { stringId: 6, note: 2, octave: 2 },
+          { stringId: 5, note: 9, octave: 2 },
+          { stringId: 4, note: 2, octave: 3 },
+          { stringId: 3, note: 7, octave: 3 },
+          { stringId: 2, note: 9, octave: 3 },
+          { stringId: 1, note: 2, octave: 4 },
+        ],
+      },
+      {
+        id: "DADF♯AD",
+        name: "Open D major / Vestapol",
+        tuning: [
+          { stringId: 6, note: 2, octave: 2 },
+          { stringId: 5, note: 9, octave: 2 },
+          { stringId: 4, note: 2, octave: 3 },
+          { stringId: 3, note: 6, octave: 3 },
+          { stringId: 2, note: 9, octave: 3 },
+          { stringId: 1, note: 2, octave: 4 },
+        ],
+      },
+      {
+        id: "DADFAD",
+        name: "Open D minor",
+        tuning: [
+          { stringId: 6, note: 2, octave: 2 },
+          { stringId: 5, note: 9, octave: 2 },
+          { stringId: 4, note: 2, octave: 3 },
+          { stringId: 3, note: 5, octave: 3 },
+          { stringId: 2, note: 9, octave: 3 },
+          { stringId: 1, note: 2, octave: 4 },
+        ],
+      },
+      {
+        id: "DADEAD",
+        name: "Dsus2",
+        tuning: [
+          { stringId: 6, note: 2, octave: 2 },
+          { stringId: 5, note: 9, octave: 2 },
+          { stringId: 4, note: 2, octave: 3 },
+          { stringId: 3, note: 4, octave: 3 },
+          { stringId: 2, note: 9, octave: 3 },
+          { stringId: 1, note: 2, octave: 4 },
+        ],
+      },
+      {
+        id: "DADDAD",
+        name: "Open D - no third",
+        tuning: [
+          { stringId: 6, note: 2, octave: 2 },
+          { stringId: 5, note: 9, octave: 2 },
+          { stringId: 4, note: 2, octave: 3 },
+          { stringId: 3, note: 2, octave: 3 },
+          { stringId: 2, note: 9, octave: 3 },
+          { stringId: 1, note: 2, octave: 4 },
+        ],
+      },
+    ],
+  },
+  {
+    cat_name: "Tunings in G",
+    tunings: [
+      {
+        id: "DGDGBD",
+        name: "Open G major",
+        tuning: [
+          { stringId: 6, note: 2, octave: 2 },
+          { stringId: 5, note: 7, octave: 2 },
+          { stringId: 4, note: 2, octave: 3 },
+          { stringId: 3, note: 7, octave: 3 },
+          { stringId: 2, note: 11, octave: 3 },
+          { stringId: 1, note: 2, octave: 4 },
+        ],
+      },
+      {
+        id: "DGDGB♭D",
+        name: "Open G minor",
+        tuning: [
+          { stringId: 6, note: 2, octave: 2 },
+          { stringId: 5, note: 7, octave: 2 },
+          { stringId: 4, note: 2, octave: 3 },
+          { stringId: 3, note: 7, octave: 3 },
+          { stringId: 2, note: 10, octave: 3 },
+          { stringId: 1, note: 2, octave: 4 },
+        ],
+      },
+    ],
+  },
+  {
+    cat_name: "Other open tunings",
+    tunings: [
+      {
+        id: "EAC♯EAE",
+        name: "Open A major",
+        tuning: [
+          { stringId: 6, note: 4, octave: 2 },
+          { stringId: 5, note: 9, octave: 2 },
+          { stringId: 4, note: 1, octave: 3 },
+          { stringId: 3, note: 4, octave: 3 },
+          { stringId: 2, note: 9, octave: 3 },
+          { stringId: 1, note: 4, octave: 4 },
+        ],
+      },
+      {
+        id: "EACEAE",
+        name: "Open A minor",
+        tuning: [
+          { stringId: 6, note: 4, octave: 2 },
+          { stringId: 5, note: 9, octave: 2 },
+          { stringId: 4, note: 0, octave: 3 },
+          { stringId: 3, note: 4, octave: 3 },
+          { stringId: 2, note: 9, octave: 3 },
+          { stringId: 1, note: 4, octave: 4 },
+        ],
+      },
+      {
+        id: "EAEACE",
+        name: "Open A minor v2",
+        tuning: [
+          { stringId: 6, note: 4, octave: 2 },
+          { stringId: 5, note: 9, octave: 2 },
+          { stringId: 4, note: 4, octave: 3 },
+          { stringId: 3, note: 9, octave: 3 },
+          { stringId: 2, note: 0, octave: 4 },
+          { stringId: 1, note: 4, octave: 4 },
+        ],
+      },
+      {
+        id: "BF♯BF♯BD♯",
+        name: "Open B major",
+        tuning: [
+          { stringId: 6, note: 11, octave: 1 },
+          { stringId: 5, note: 6, octave: 2 },
+          { stringId: 4, note: 11, octave: 2 },
+          { stringId: 3, note: 6, octave: 3 },
+          { stringId: 2, note: 11, octave: 3 },
+          { stringId: 1, note: 3, octave: 4 },
+        ],
+      },
+      {
+        id: "CFCFAC",
+        name: "Open F major",
+        tuning: [
+          { stringId: 6, note: 0, octave: 2 },
+          { stringId: 5, note: 5, octave: 2 },
+          { stringId: 4, note: 0, octave: 3 },
+          { stringId: 3, note: 5, octave: 3 },
+          { stringId: 2, note: 9, octave: 3 },
+          { stringId: 1, note: 0, octave: 4 },
+        ],
+      },
+    ],
+  },
+  {
+    cat_name: "Other modal & extended tunings",
+    tunings: [
+      {
+        id: "BEBEBE",
+        name: "Nick Drake's B modal",
+        tuning: [
+          { stringId: 6, note: 11, octave: 1 },
+          { stringId: 5, note: 4, octave: 2 },
+          { stringId: 4, note: 11, octave: 2 },
+          { stringId: 3, note: 4, octave: 3 },
+          { stringId: 2, note: 11, octave: 3 },
+          { stringId: 1, note: 4, octave: 4 },
+        ],
+      },
+      {
+        id: "CACGCE",
+        name: "C6",
+        tuning: [
+          { stringId: 6, note: 0, octave: 2 },
+          { stringId: 5, note: 9, octave: 2 },
+          { stringId: 4, note: 0, octave: 3 },
+          { stringId: 3, note: 7, octave: 3 },
+          { stringId: 2, note: 0, octave: 4 },
+          { stringId: 1, note: 4, octave: 4 },
+        ],
+      },
+      {
+        id: "CGDGCD",
+        name: "Gsus4 / Orkney Tuning",
+        tuning: [
+          { stringId: 6, note: 0, octave: 2 },
+          { stringId: 5, note: 7, octave: 2 },
+          { stringId: 4, note: 2, octave: 3 },
+          { stringId: 3, note: 7, octave: 3 },
+          { stringId: 2, note: 0, octave: 4 },
+          { stringId: 1, note: 2, octave: 4 },
+        ],
+      },
+      {
+        id: "DGDGBE",
+        name: "G6",
+        tuning: [
+          { stringId: 6, note: 2, octave: 2 },
+          { stringId: 5, note: 7, octave: 2 },
+          { stringId: 4, note: 2, octave: 3 },
+          { stringId: 3, note: 7, octave: 3 },
+          { stringId: 2, note: 11, octave: 3 },
+          { stringId: 1, note: 4, octave: 4 },
+        ],
+      },
+    ],
+  },
+  {
+    cat_name: "Other",
+    tunings: [
+      {
+        id: "BF♯BGBE",
+        name: "Karnivool Tuning",
+        tuning: [
+          { stringId: 6, note: 11, octave: 1 },
+          { stringId: 5, note: 6, octave: 2 },
+          { stringId: 4, note: 11, octave: 2 },
+          { stringId: 3, note: 7, octave: 3 },
+          { stringId: 2, note: 11, octave: 3 },
+          { stringId: 1, note: 4, octave: 4 },
+        ],
+      },
+      {
+        id: "CGCFCE",
+        name: "Pink Moon",
+        tuning: [
+          { stringId: 6, note: 0, octave: 2 },
+          { stringId: 5, note: 7, octave: 2 },
+          { stringId: 4, note: 0, octave: 3 },
+          { stringId: 3, note: 5, octave: 3 },
+          { stringId: 2, note: 0, octave: 4 },
+          { stringId: 1, note: 4, octave: 4 },
+        ],
+      },
+      {
+        id: "CGDAEG",
+        name: "Fripp's New Standard Tuning",
+        tuning: [
+          { stringId: 6, note: 0, octave: 2 },
+          { stringId: 5, note: 7, octave: 2 },
+          { stringId: 4, note: 2, octave: 3 },
+          { stringId: 3, note: 9, octave: 3 },
+          { stringId: 2, note: 4, octave: 4 },
+          { stringId: 1, note: 7, octave: 4 },
+        ],
+      },
+      {
+        id: "DADEBC♯",
+        name: "Ålesund",
+        tuning: [
+          { stringId: 6, note: 2, octave: 2 },
+          { stringId: 5, note: 9, octave: 2 },
+          { stringId: 4, note: 2, octave: 3 },
+          { stringId: 3, note: 4, octave: 3 },
+          { stringId: 2, note: 11, octave: 3 },
+          { stringId: 1, note: 1, octave: 4 },
+        ],
+      },
+      {
+        id: "DADF♯BE",
+        name: "José González tuning",
+        tuning: [
+          { stringId: 6, note: 2, octave: 2 },
+          { stringId: 5, note: 9, octave: 2 },
+          { stringId: 4, note: 2, octave: 3 },
+          { stringId: 3, note: 6, octave: 3 },
+          { stringId: 2, note: 11, octave: 3 },
+          { stringId: 1, note: 4, octave: 4 },
+        ],
+      },
+      {
+        id: "DGCGCD",
+        name: "The Rain Song",
+        tuning: [
+          { stringId: 6, note: 2, octave: 2 },
+          { stringId: 5, note: 7, octave: 2 },
+          { stringId: 4, note: 0, octave: 3 },
+          { stringId: 3, note: 7, octave: 3 },
+          { stringId: 2, note: 0, octave: 4 },
+          { stringId: 1, note: 2, octave: 4 },
+        ],
+      },
+    ],
+  },
+];
+
+export default tuningPresets;
