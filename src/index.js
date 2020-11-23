15f470ef9e3af19dc296bb69b3059a0d31048b3c
Rémy Turpault <remy.turpault@gmail.com>
2020-11-23 02:26:28 +0100

    disable console in production

--- src/index.js
+++ src/index.js
@@ -11,29 +11,33 @@ import App from "./App";
 import store from "./store";
 import * as serviceWorker from "./serviceWorker";
 
+if (process.env.NODE_ENV !== "development") {
+	console.log = () => {};
+}
+
 const theme = createMuiTheme({
-  typography: {
-    fontFamily: `'M PLUS 1p', sans-serif`,
-  },
-  palette: {
-    primary: {
-      main: "#468189",
-    },
-    secondary: {
-      main: "#e54b4b",
-    },
-  },
+	typography: {
+		fontFamily: `'M PLUS 1p', sans-serif`,
+	},
+	palette: {
+		primary: {
+			main: "#468189",
+		},
+		secondary: {
+			main: "#e54b4b",
+		},
+	},
 });
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById("root")
+	<Provider store={store}>
+		<BrowserRouter>
+			<ThemeProvider theme={theme}>
+				<App />
+			</ThemeProvider>
+		</BrowserRouter>
+	</Provider>,
+	document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
